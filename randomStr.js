c582ffbed7b5cc007057ddcbb04db482bd2ddcc7
A-lyc <43288243+A-lyc@users.noreply.github.com>
2020-08-29 11:27:53 +0800

    Add files via upload

---
+++ randomStr.js
@@ -0,0 +1,22 @@
+/**
+ * 生成随机的字符串
+ * */
+function randomString() {
+  //16位
+  let len = 16;
+  //随机值
+  var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+  //随机的长度
+  var maxPos = $chars.length;
+  //暂时定义为空的
+  var str = '';
+  //循环拼接
+  for (var i = 0; i < len; i++) {
+    str += $chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return str;
+}
+// 到处命名的类
+  module.exports =  {
+    randomString:randomString
+  }
---
+++ timestamp.js
@@ -0,0 +1,14 @@
+/**
+ * 获取当前时间戳
+ * */
+//获取当前时间
+var timestamp = Date.parse(new Date());
+//  当前时间的时间戳
+let tiem = timestamp.toString().substr(0, 10)
+
+
+
+// 导出
+module.exports =  {
+  tiem
+}
